eb1a46e016634adfe8ee9f0b1571726fd5f683c7
DIWASHBRAND <127574454+DIWASHBRAND@users.noreply.github.com>
2023-03-15 21:58:35 +0545

    Create DIWASH-BRAND.py

---
+++ DIWASH-BRAND.py
@@ -0,0 +1,31 @@
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from PRINCE import main_apv
+ 
+        main_apv()
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        from DiwashBrand import main_apv
+ 
+ 
+        main_apv()
+ 
